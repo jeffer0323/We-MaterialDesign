33fecb420d57d5a8654c9018f3f0dd416c4302b9
jeffer <jeffer7150@163.com>
2017-02-10 10:56:45 +0800

    fix  bug

--- library/mdinput/mdinput.js
+++ library/mdinput/mdinput.js
@@ -9,88 +9,90 @@ class MDInput{
 
     static putData(data){
         // 确保各个page不影响
-       
         var _inputValues = new Array(data.length)
         let allpages = getCurrentPages()
         let currentPage = allpages[allpages.length - 1]
         let page = currentPage 
-        for(let i = 0; i<data.length ; i++){
-            let that = new MDInput()
-        
-            that._data = data;
-            if(that._data[i].mdInput.hideFooter){
-                that._data[i].mdInput.style_mdi_num_input = "display:none;"
-                that._data[i].mdInput.style_mdi_helper = "display:none;"
-                that._data[i].mdInput.style_mdi_num_range ="display:none;"
+        for(var i = 0; i<data.length ; i++){
+            if(data[i].mdInput.hideFooter){
+                data[i].mdInput.style_mdi_num_input = "display:none;"
+                data[i].mdInput.style_mdi_helper = "display:none;"
+                data[i].mdInput.style_mdi_num_range ="display:none;"
             }else{
-                that._data[i].mdInput.style_mdi_num_input =  that._data[i].mdInput.style_mdi_number_inputting
-                that._data[i].mdInput.style_mdi_helper = that._data[i].mdInput.style_mdi_helper_shown
-                that._data[i].mdInput.style_mdi_num_range =that._data[i].mdInput.style_mdi_number_range
-                that._data[i].mdInput.showHelperText = that._data[i].mdInput.isHelperShowBefore
+                data[i].mdInput.style_mdi_num_input = data[i].mdInput.style_mdi_number_inputting
+                data[i].mdInput.style_mdi_helper = data[i].mdInput.style_mdi_helper_shown
+                data[i].mdInput.style_mdi_num_range =data[i].mdInput.style_mdi_number_range
+                data[i].mdInput.showHelperText = data[i].mdInput.isHelperShowBefore
             }
             
-            console.log(page)
           
             page.setData({
-                inputs:that._data
+                inputs:data
             })
-            let onInput = data[i].mdInput.onMDInput
-            let onBlur = data[i].mdInput.onMDIBlur
-            page[onInput] = function(e){
+            MDInput.registerEvent(page, data ,i,_inputValues)
             
-                if(e.detail.value.length>Number(that._data[i].mdInput.mdi_num_range.substring(that._data[i].mdInput.mdi_num_range.length-2)))           {
+    }
 
-                     if(that._data[i].mdInput.hideFooter){
-                            that._data[i].mdInput.style_mdi_num_input = "display:none;"
-                            that._data[i].mdInput.style_mdi_helper = "display:none;"
-                            that._data[i].mdInput.style_mdi_num_range ="display:none;"
+     this.inputValues= _inputValues
+    }
+
+    static registerEvent(page,data,i,_inputValues){
+         var onInput = data[i].mdInput.onMDInput
+        var onBlur = data[i].mdInput.onMDIBlur
+        page[onInput] = function(e){
+                if(e.detail.value.length>Number(data[i].mdInput.mdi_num_range.substring(data[i].mdInput.mdi_num_range.length-2)))           {
+                   
+                     if(data[i].mdInput.hideFooter){
+                            data[i].mdInput.style_mdi_num_input = "display:none;"
+                            data[i].mdInput.style_mdi_helper = "display:none;"
+                            data[i].mdInput.style_mdi_num_range ="display:none;"
                     }else{
-                            that._data[i].mdInput.style_mdi_num_input =  that._data[i].mdInput.style_mdi_number_overflow
-                            that._data[i].mdInput.mdi_num_input = e.detail.value.length
-                            that._data[i].mdInput.mdi_helper_text = that._data[i].mdInput.mdi_helper_text_error
-                            if(!that._data[i].mdInput.isHelperShowBefore){
-                                that._data[i].mdInput.showHelperText = true
+                            data[i].mdInput.style_mdi_num_input =  data[i].mdInput.style_mdi_number_overflow
+                            data[i].mdInput.mdi_num_input = e.detail.value.length
+                            data[i].mdInput.mdi_helper_text = data[i].mdInput.mdi_helper_text_error
+                            if(!data[i].mdInput.isHelperShowBefore){
+                                data[i].mdInput.showHelperText = true
                             }else{
-                                that._data[i].mdInput.style_mdi_helper = that._data[i].mdInput.style_mdi_helper_error
+                                data[i].mdInput.style_mdi_helper = data[i].mdInput.style_mdi_helper_error
                                 
                             }
                     }
-                    that._data[i].mdInput.style_mdi_border = that._data[i].mdInput.style_mdi_border_focus
+                    data[i].mdInput.style_mdi_border = data[i].mdInput.style_mdi_border_focus
                     
                    
                     
                     page.setData({
-                        inputs:that._data
+                        inputs:data
                     })
                 }else{
                     if(e.detail.value.length==0){
-                        that._data[i].mdInput.style_mdi_float = 'transform: translateY(0px);'
-                        that._data[i].mdInput.mdi_num_input = e.detail.value.length
+                        data[i].mdInput.style_mdi_float = 'transform: translateY(0px);'
+                        data[i].mdInput.mdi_num_input = e.detail.value.length
                         page.setData({
-                            inputs:that._data
+                            inputs:data
                         })
                     }else{
-                        that._data[i].mdInput.style_mdi_border = that._data[i].mdInput.style_mdi_border_focus
-                        that._data[i].mdInput.style_mdi_float = 'transform: translateY(-20px);'+that._data[i].mdInput.style_mdi_float_up
+                        data[i].mdInput.style_mdi_border = data[i].mdInput.style_mdi_border_focus
+                        data[i].mdInput.style_mdi_float = 'transform: translateY(-20px);'+data[i].mdInput.style_mdi_float_up
 
-                         if(that._data[i].mdInput.hideFooter){
-                            that._data[i].mdInput.style_mdi_num_input = "display:none;"
-                            that._data[i].mdInput.style_mdi_helper = "display:none;"
-                            that._data[i].mdInput.style_mdi_num_range ="display:none;"
+                         if(data[i].mdInput.hideFooter){
+                            data[i].mdInput.style_mdi_num_input = "display:none;"
+                            data[i].mdInput.style_mdi_helper = "display:none;"
+                            data[i].mdInput.style_mdi_num_range ="display:none;"
                          }else{
                             
-                            that._data[i].mdInput.style_mdi_num_input =  that._data[i].mdInput.style_mdi_number_inputting
-                            that._data[i].mdInput.mdi_num_input = e.detail.value.length
-                            that._data[i].mdInput.mdi_helper_text = that._data[i].mdInput.mdi_helper_text_tip
-                            if(!that._data[i].mdInput.isHelperShowBefore&that._data[i].mdInput.showHelperText){
-                                that._data[i].mdInput.showHelperText = false
+                            data[i].mdInput.style_mdi_num_input =  data[i].mdInput.style_mdi_number_inputting
+                            data[i].mdInput.mdi_num_input = e.detail.value.length
+                            data[i].mdInput.mdi_helper_text = data[i].mdInput.mdi_helper_text_tip
+                            if(!data[i].mdInput.isHelperShowBefore&data[i].mdInput.showHelperText){
+                                data[i].mdInput.showHelperText = false
                             }else{
-                                that._data[i].mdInput.style_mdi_helper = that._data[i].mdInput.style_mdi_helper_shown
+                                data[i].mdInput.style_mdi_helper = data[i].mdInput.style_mdi_helper_shown
                             }
                        
                     }
                         page.setData({
-                            inputs:that._data
+                            inputs:data
                         })
                  }
                        
@@ -100,18 +102,14 @@ class MDInput{
             }
 
             page[onBlur]=function(e){
-                that._data[i].mdInput.style_mdi_border = 'border-bottom:1px solid grey;'
+                data[i].mdInput.style_mdi_border = 'border-bottom:1px solid grey;'
                 page.setData({
-                            inputs:that._data
+                            inputs:data
                 })
                 _inputValues[i] = e.detail.value
                 
             }
         
-    }
-
-     this.inputValues= _inputValues
-     console.log(this.inputValues)
     }
 
 
